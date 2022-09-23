1ef2d4d9b973795c87cb914762040dca9fb4e62b
Vibi31 <vrvichu0@gmail.com>
2022-09-23 14:53:34 +0200

    small corrections

--- problem3c.cpp
+++ problem3c.cpp
@@ -5,15 +5,18 @@
 #include <fstream>
 
 #include <armadillo>    //include our armadillo package and armadillo matricy form text file A.txt
-mat A;
-A.load("A.txt");
+
 
 using namespace arma;
 using namespace std;
 
-int main(mat){
-    int rows =  sizeof ary / sizeof mat[0]; // 2 rows  
-    int cols = sizeof mat[0] / sizeof(int); // 5 cols
+int main(){
+    
+    mat A;
+    A.load("A.txt");
+
+    int rows =  sizeof ary / sizeof mat[0]; // rows  
+    int cols = sizeof mat[0] / sizeof(int); // cols
     int a = 0
     for (int i = 0; i < rows; i++) { 
         i = i;
@@ -21,13 +24,10 @@ int main(mat){
             j = j;
             if (i == j) {
                 if(int mat[i][j] > a);
-                    int a = mat[i][j]
+                    a = mat[i][j]
                     } ;
         }
      }
     cout << "Largest value in matrix is: " << a;
     return a
 }
-
-int rows =  sizeof ary / sizeof mat[0]; // 2 rows  
-int cols = sizeof mat[0] / sizeof(int); // 5 cols
