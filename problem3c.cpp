579afb3cb660b71611cd97233ab23b07d3ee1184
Vibi31 <vrvichu0@gmail.com>
2022-09-23 14:27:35 +0200

    finished task 3 i think, need to test it out though

--- problem3c.cpp
+++ problem3c.cpp
@@ -2,6 +2,32 @@
 #include <vector>
 #include <cmath>
 #include <string.h>
-#include<fstream>
+#include <fstream>
 
+#include <armadillo>    //include our armadillo package and armadillo matricy form text file A.txt
+mat A;
+A.load("A.txt");
+
+using namespace arma;
 using namespace std;
+
+int main(mat){
+    int rows =  sizeof ary / sizeof mat[0]; // 2 rows  
+    int cols = sizeof mat[0] / sizeof(int); // 5 cols
+    int a = 0
+    for (int i = 0; i < rows; i++) { 
+        i = i;
+        for (int j = 0; j < cols; j++) {
+            j = j;
+            if (i == j) {
+                if(int mat[i][j] > a);
+                    int a = mat[i][j]
+                    } ;
+        }
+     }
+    cout << "Largest value in matrix is: " << a;
+    return a
+}
+
+int rows =  sizeof ary / sizeof mat[0]; // 2 rows  
+int cols = sizeof mat[0] / sizeof(int); // 5 cols
