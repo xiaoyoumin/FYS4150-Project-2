42754b121c5fe9cb733094b513f1299e4e116011
Vibi31 <vrvichu0@gmail.com>
2022-09-27 20:25:56 +0200

    changed the file completely

--- problem3.cpp
+++ problem3.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <string.h>
-#include <fstream>
-
 #include <armadillo>    //include our armadillo package and armadillo matricy form text file A.txt
-
 
 using namespace arma;
 using namespace std;
 
-int main(){
-    
-    mat A;
-    A.load("A.txt");
+// has return type double
+// takes an Armadillo matrix as input
+// takes references to two integers as input
+// identifies the largest off-diagonal element (in absolute value) in the matrix, under the assumption of a symmetric matrix
+// writes the matrix indices for this element to the two integer references
+// returns the value of this matrix element
 
-    int rows =  sizeof ary / sizeof mat[0]; // rows  
-    int cols = sizeof mat[0] / sizeof(int); // cols
-    float a = 0
-    for (int i = 0; i < rows; i++) { 
-        for (int j = 0; j < cols; j++) {
-            if (i == j) {
-                if(int mat[i][j] > a);
-                    a = mat[i][j]
-                    } ;
+
+double max_offdiag_symmetric(const arma::mat& A, int& k, int &l)        //to create a return type double taking in matrix A and int k, l
+{
+ 
+    int rows = A.n_rows;                                // rows  
+    int cols =  A.n_cols;                               // coloumns
+    
+    if (rows!= cols){                                   //checking if dimensions match
+        std::cout << "Dimensions are not equal!";
         }
-     }
-    cout << "Largest value in matrix is: " << a;
-    return a
-}
+
+    int a = 0;
+
+    for (int i = 0; i < rows; i++) {                     //nested for loop to iterate through matrix location
+        for (int j = 0; j < cols; j++){
+            if (i!= j){                                  //making sure its off-diagonal element
+                if (abs(A(i,j)) > a ){                   
+                    double a = abs(A(i,j));              //updating our largest off value = a
+                    int k = i;                            //
+                    int l = j; 
+                    }
+                }
+            }
+        } 
+
+        
+    cout << "position is :" << "(\\l , \\k)";
+    cout << "Largest value in matrix is: " << a;         //printing out our largest off diagonal value
+    return a;                                            //returning the value to whomever called the function (double data type)
+    }
