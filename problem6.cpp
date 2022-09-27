ebb9621518d2e95eb5087e78f302040f2c70631c
david <david.svejda2000@gmail.com>
2022-09-27 22:29:20 +0200

    Wrote the cpp code for problem 6, can't test it

--- problem6.cpp
+++ problem6.cpp
@@ -1,43 +1,52 @@
 
 #include <iostream>
 #include <string>
-// #include <armadillo>
-// #include <fstream>
-// #include <experimental/filesystem>
+#include <armadillo>
+#include <fstream>
+#include <experimental/filesystem>
 
-// #include "JCB_rota_Algr/EigSym_JcbAgrm.h"
-// namespace fs = std::experimental::filesystem;
+#include "JCB_rota_Algr/EigSym_JcbAgrm.h"
+namespace fs = std::experimental::filesystem;
 
-// void write_to_file(arma::mat& matrix, std::string filename, int nvectors){
-//     fs::create_directories("./output");
-//     std::ofstream file;
-//     file.open("output/"+filename+".txt", std::ios::trunc);
-//     // file << nvectors << "\t" << matrix.n_rows;
-//     for(int i=0; i<matrix.n_rows; i++)
-//     {
-//         // for(int j=0; j<nvectors-1; j++)
-//         // {   file << matrix[i,j] <<"\t";}
-//         // file << matrix[i,nvectors-1] <<"\n";
-//     }
-//     file.close();
-// }
+void write_to_file(arma::mat& matrix, std::string filename, int nvectors){
+    //make the output directory if it doesn't exist yet
+    fs::create_directories("./output");
+    //open the file
+    std::ofstream file;
+    file.open("output/"+filename+".txt", std::ios::trunc);
+    //make a header with the numer of columns and rows
+    file << nvectors << "\t" << matrix.n_rows;
+    //write the data
+    for(int i=0; i<matrix.n_rows; i++)
+    {
+        for(int j=0; j<nvectors-1; j++)
+        {   file << matrix[i,j] <<"\t";}
+        file << matrix[i,nvectors-1] <<"\n";
+    }
+    file.close();
+}
 
+int calculate_eigenvectors(int n){
+    //Do the calculations and write the results to a file
+    arma::mat A;
+    double h = 1/n;
+    double h2 = 1/(h*h);
+    A = create_tridiagonal(n,-h2,2*h2,-h2);
+    double eps = 1E-8;
+    arma::vec eigenvalues;
+    arma::mat eigenvectors;
+    int iterations;
+    bool converged;
+
+    void jacobi_eigensolver(A, eps, eigenvalues, eigenvectors, n*n, iterations, converged);
+
+    write_to_file(eigenvectors, "eigenvectors_" + string(n), 3);
+    return 0;
+}
 
 int main(){
-    //Pseudo code
-
-    //import the algorithm
-    //??
-    // arma::mat A;
-    // A = create_tridiagonal(4,1,2,3);
-
-    // write_to_file(A, "test", 3);
-
-    //void jacobi_eigensolver(const mat& A, double eps, vec& eigenvalues, mat& eigenvectors,
-    //	const int maxiter, int& iterations, bool& converged)
-
-    //Calculate the plot for n = 10 and n = 100
-
-    //Write the results to two files
+    //perform the cases for n = 10 and n = 100
+    calculate_eigenvectors(10);
+    calculate_eigenvectors(100);
     return 0;
 }
